e85e9ec84b4763e2b3b1dec80b1cf9b012aa6937
Jeroen Visscher <jeroen.visscher@HRorganizer.com>
2025-01-05 12:11:14 +0000

    footer, mission, summaries

--- src/navigation.ts
+++ src/navigation.ts
@@ -132,54 +132,24 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
+    {}, {}, {
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
+        { text: 'Samenvatting', href: '/samenvatting'},
+        { text: 'Summary', href: '/summary'},
+      ]
     },
     {
-      title: 'Platform',
+      title: 'Stichting',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Missie', href: '/missie' },
+        { text: 'AVG', href: '/privacy' },
+        { text: 'Contact', href: '/contact' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Missie', href: getPermalink('/missie') },
+    { text: 'AVG Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     { ariaLabel: 'Google groups', icon: 'tabler:brand-google', href: 'https://groups.google.com/g/aikidoarnhem' },
